9c2b75d380eae394e18fa1ea09d50b55d242c836
NabeelJaveed123 <94675988+NabeelJaveed123@users.noreply.github.com>
2023-11-24 08:54:44 +0500

    Update AdapterPatternDemo.java

--- Adapter/LabTask/AdapterPatternDemo.java
+++ Adapter/LabTask/AdapterPatternDemo.java
@@ -2,12 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package GOF.StructuralPattern.Adapter.LabTask;
+package Adapter.LabTask;
 
-/**
- *
- * @author shafq
- */
+
+
 public class AdapterPatternDemo {
     public static void main(String[] args) {
         AudioPlayer audioPlayer = new AudioPlayer();
