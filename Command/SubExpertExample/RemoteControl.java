a46a1dd8e29099c255446e80de783cb8acf42622
NabeelJaveed123 <94675988+NabeelJaveed123@users.noreply.github.com>
2023-11-24 09:48:40 +0500

    Update RemoteControl.java

--- Command/SubExpertExample/RemoteControl.java
+++ Command/SubExpertExample/RemoteControl.java
@@ -3,12 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package GOF.BehavioralPattern.Command.SubExpertExample;
+package Command.SubExpertExample;
 
-/**
- *
- * @author FA20-BSE-042
- */
+
 //Invoker
 
 public class RemoteControl {
