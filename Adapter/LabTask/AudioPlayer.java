79ef23392fbbfad0d404af5c0ab7c86e03725862
NabeelJaveed123 <94675988+NabeelJaveed123@users.noreply.github.com>
2023-11-24 08:55:17 +0500

    Update AudioPlayer.java

--- Adapter/LabTask/AudioPlayer.java
+++ Adapter/LabTask/AudioPlayer.java
@@ -2,12 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package GOF.StructuralPattern.Adapter.LabTask;
+packageAdapter.LabTask;
 
-/**
- *
- * @author shafq
- */
 
 public class AudioPlayer implements MediaPlayer {
     MediaAdapter mediaAdapter;
