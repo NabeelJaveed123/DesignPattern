8bdcae6c844fb46281b0cd8898c6d2bc8c53ecce
NabeelJaveed123 <94675988+NabeelJaveed123@users.noreply.github.com>
2023-11-24 08:54:15 +0500

    Update AacPlayer.java

--- Adapter/LabTask/AacPlayer.java
+++ Adapter/LabTask/AacPlayer.java
@@ -2,12 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package GOF.StructuralPattern.Adapter.LabTask;
+package Adapter.LabTask;
 
-/**
- *
- * @author shafq
- */
+
 
 
 public class AacPlayer implements AdvancedMediaPlayer {
