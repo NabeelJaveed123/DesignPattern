908ccb1d1e86650750390f3de4c07e57c2c0c024
NabeelJaveed123 <94675988+NabeelJaveed123@users.noreply.github.com>
2023-11-24 08:56:03 +0500

    Update Mp4Player.java

--- Adapter/LabTask/Mp4Player.java
+++ Adapter/LabTask/Mp4Player.java
@@ -2,12 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package GOF.StructuralPattern.Adapter.LabTask;
+package Adapter.LabTask;
 
-/**
- *
- * @author shafq
- */
+
 public class Mp4Player implements AdvancedMediaPlayer{
 
    @Override
