c6ff7037d0dc68dc3fb1341ae0e5b0453467c901
NabeelJaveed123 <94675988+NabeelJaveed123@users.noreply.github.com>
2023-11-24 09:26:00 +0500

    Update ProxyPatternDemo.java

--- Proxy/LabTask/ProxyPatternDemo.java
+++ Proxy/LabTask/ProxyPatternDemo.java
@@ -3,12 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package GOF.StructuralPattern.Proxy.LabTask;
+packageProxy.LabTask;
 
-/**
- *
- * @author FA20-BSE-042
- */
 public class ProxyPatternDemo {
 	
     public static void main(String[] args) {
