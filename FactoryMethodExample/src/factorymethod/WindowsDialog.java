05189c812a37e1629864ec21b92d5ad40751852c
NabeelJaveed123 <94675988+NabeelJaveed123@users.noreply.github.com>
2023-12-22 09:35:59 +0500

    Add files via upload

---
+++ FactoryMethodExample/src/factorymethod/WindowsDialog.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorymethod;
+
+/**
+ *
+ * @author fa20-bse-061
+ */
+public class WindowsDialog extends Dialog {
+
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+}
